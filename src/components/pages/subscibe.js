3fdb578f1e66ab5ec6327bba08de3ad775eedaf5
itzhord <mhiztafranck@mail.com>
2024-03-26 12:39:46 +0100

    Fixed some fonts

--- src/components/pages/subscibe.js
+++ src/components/pages/subscibe.js
@@ -75,13 +75,13 @@ const Subscribe = () => {
       </div>
       <div className="h-[65vh] w-[100] bg-blue-400">
         <div className="relative text-white font-bold left-[20%] top-[7rem]">
-        <h2 className=" font-sans text-[30px] mb-2"> Stay in touch with us </h2>
-        <p className="font-sans text-[15px] mb-1"> To get our latest offers and services </p >
-        <p className="font-sans text-[15px] font">On our react packages that meets your needs</p>
+        <h2 className="  text-[30px] mb-2"> Stay in touch with us </h2>
+        <p className=" text-[15px] mb-1"> To get our latest offers and services </p >
+        <p className=" text-[15px] font">On our react packages that meets your needs</p>
         </div>
       </div>
       <div className="w-[60%] h-[40vh] flex flex-row relative left-[20%] bottom-[12rem] bg-slate-400">
-          <div className="flex flex-col w-3/5 h-[40vh] pl-[70px] pt-[30px] bg-white font-sans ">
+          <div className="flex flex-col w-3/5 h-[40vh] pl-[70px] pt-[30px] bg-white  ">
             <h3 className="font-bold text-blue-800">Kindly Provide these infomation</h3>
             <div className="flex flex-col ">
                 <div>
@@ -117,9 +117,9 @@ const Subscribe = () => {
              </div>
             </div>
             
-            <button onClick={handleSubmit} className="border-[3px] w-[20%] bg-blue-800 mt-4 text-white py-[4px] px-[6px] font-sans font-semibold ml-[17rem] mt-[1rem] rounded-2xl ">Submit</button>
+            <button onClick={handleSubmit} className="border-[3px] w-[20%] bg-blue-800 mt-4 text-white py-[4px] px-[6px] font-semibold ml-[17rem] mt-[1rem] rounded-2xl ">Submit</button>
           </div>
-          <div className="w-2/5 h-[40vh] p-[70px] font-sans font-semibold text-white bg-blue-950">
+          <div className="w-2/5 h-[40vh] p-[70px]  font-semibold text-white bg-blue-950">
             <h2 className="mb-5">This is our information</h2>
             <h2 className="mb-5">Nigeria</h2>
             <h2>Port Harcourt</h2>
